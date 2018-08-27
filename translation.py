c839c32aaeb8aa4a2f9913bc51b1fdd46cf6dd16
Stanislav Baltrunas <stanislav@baltrunas.ru>
2018-08-27 18:56:32 +0600

    https

--- translation.py
+++ translation.py
@@ -88,7 +88,7 @@ class PageAdmin(TranslationAdmin):
 	class Media:
 		js = (
 			'modeltranslation/js/force_jquery.js',
-			'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
+			'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
 			'modeltranslation/js/tabbed_translation_fields.js',
 		)
 		css = {
